e77fce56524d93224b2e75d2f1573e7a107f92d7
Jannik Schmiedl <moin@jannik.schmiedl.de>
2020-02-25 08:13:10 +0100

    extract config

---
+++ src/config.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Config struct {
+	BucketID    string
+	Host        string
+	Port        string
+	ArchiveDir  string
+	ArchiveName string
+	AuthPart    string
+	DB          string
+}
+
+func initConfig() Config {
+	bucketID := os.Getenv("GCS_BUCKET")
+	if bucketID == "" {
+		log.Panic("bucket id can not be empty, set it as environment variable GCS_BUCKET")
+	}
+
+	var (
+		host        = "localhost"
+		port        = "27017"
+		dir         = "/tmp"
+		archiveName = fmt.Sprintf("backup-%s.tar.gz", time.Now().Format(time.RFC3339))
+		auth        string
+		db          string
+	)
+
+	if os.Getenv("MONGODB_HOST") != "" {
+		host = os.Getenv("MONGODB_HOST")
+	}
+
+	if os.Getenv("MONGODB_PORT") != "" {
+		port = os.Getenv("MONGODB_PORT")
+	}
+
+	if os.Getenv("BACKUP_DIR") != "" {
+		dir = os.Getenv("BACKUP_DIR")
+	}
+
+	if os.Getenv("MONGODB_USER") != "" && os.Getenv("MONGODB_PASSWORD") != "" {
+		auth = fmt.Sprintf("--username=%s --password=%s", os.Getenv("MONGODB_USER"), os.Getenv("MONGODB_PASSWORD"))
+	}
+
+	if os.Getenv("MONGODB_DB") != "" {
+		db = fmt.Sprintf("--db=%s", os.Getenv("MONGODB_DB"))
+	}
+
+	return Config{
+		BucketID:    bucketID,
+		Host:        host,
+		Port:        port,
+		ArchiveDir:  dir,
+		ArchiveName: archiveName,
+		AuthPart:    auth,
+		DB:          db,
+	}
+}
--- src/main.go
+++ src/main.go
@@ -3,27 +3,13 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"os/exec"
-	"time"
 
 	"github.com/Sandburg/mongodb-backup/src/storage"
-	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
-type Config struct {
-	BucketID    string
-	Host        string
-	Port        string
-	ArchiveDir  string
-	ArchiveName string
-	AuthPart    string
-	DB          string
-}
-
 func main() {
-	godotenv.Load()
 	c := initConfig()
 
 	storage := storage.New(c.BucketID)
@@ -52,50 +38,4 @@ func main() {
 	}
 
 	storage.WriteFile(c.ArchiveName, dumpData)
-}
-
-func initConfig() Config {
-	bucketID := os.Getenv("GCS_BUCKET")
-	if bucketID == "" {
-		log.Panic("bucket id can not be empty, set it as environment variable GCS_BUCKET")
-	}
-
-	var (
-		host        = "localhost"
-		port        = "27017"
-		dir         = "/tmp"
-		archiveName = fmt.Sprintf("backup-%s.tar.gz", time.Now().Format(time.RFC3339))
-		auth        string
-		db          string
-	)
-
-	if os.Getenv("MONGODB_HOST") != "" {
-		host = os.Getenv("MONGODB_HOST")
-	}
-
-	if os.Getenv("MONGODB_PORT") != "" {
-		port = os.Getenv("MONGODB_PORT")
-	}
-
-	if os.Getenv("BACKUP_DIR") != "" {
-		dir = os.Getenv("BACKUP_DIR")
-	}
-
-	if os.Getenv("MONGODB_USER") != "" && os.Getenv("MONGODB_PASSWORD") != "" {
-		auth = fmt.Sprintf("--username=%s --password=%s", os.Getenv("MONGODB_USER"), os.Getenv("MONGODB_PASSWORD"))
-	}
-
-	if os.Getenv("MONGODB_DB") != "" {
-		db = fmt.Sprintf("--db=%s", os.Getenv("MONGODB_DB"))
-	}
-
-	return Config{
-		BucketID:    bucketID,
-		Host:        host,
-		Port:        port,
-		ArchiveDir:  dir,
-		ArchiveName: archiveName,
-		AuthPart:    auth,
-		DB:          db,
-	}
 }
