36443d963ac6870d8ae267e1868748488aad30c8
Jannik Schmiedl <moin@jannik.schmiedl.de>
2020-02-25 12:28:37 +0100

    wrap arguments in quotes

--- src/main.go
+++ src/main.go
@@ -19,11 +19,11 @@ func main() {
 
 	cmd := exec.Command(
 		"mongodump",
-		fmt.Sprintf("--host %s", c.Host),
-		fmt.Sprintf("--port %s", c.Port),
+		fmt.Sprintf(`--host="%s"`, c.Host),
+		fmt.Sprintf("--port=%s", c.Port),
 		fmt.Sprintf("%s", c.AuthPart),
 		fmt.Sprintf("%s", c.DB),
-		fmt.Sprintf("--archive %s/%s", c.ArchiveDir, c.ArchiveName),
+		fmt.Sprintf(`--archive="%s/%s"`, c.ArchiveDir, c.ArchiveName),
 	)
 
 	log.Infof("Starting mongodump: %s", cmd.String())
